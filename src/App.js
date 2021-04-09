83cf49d40b454cc1e4a2800bf36a2d55bb036cc1
Varfolomeev-Ilya <ilya323472@yandex.ru>
2021-04-09 09:48:16 +0300

    fix bugs with reload pages

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 import React from 'react';
 import Routers from '../src/Router/Router';
-// import { regUser } from '../src/store/users';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { tokenChecker } from '../src//api/authApi';
+import { regUser } from '../src/store/users';
+import { useDispatch } from 'react-redux';
+import { tokenChecker } from '../src//api/authApi';
 
 function App() {
-  // const dispatch = useDispatch();
-  // const userInfo = useSelector((state) => state.users.registeredUser);
-  // const id = userInfo.id;
-  // console.log(id)
-  // const [isLoading, setIsLoading] = React.useState(false);
-  // const checkToken = async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     const response = await tokenChecker(id);
-  //     if (response.data) {
-  //       console.log(response)
-  //       dispatch(regUser(response.data));
-  //     } else {
-  //       localStorage.removeItem('isAuthenticated');
-  //     }
-  //   } catch (error) {
-  //     alert(error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
-  // React.useEffect(() => {
-  //   checkToken();
-  // }, []);
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = React.useState(false);
+  const checkToken = async () => {
+    setIsLoading(true);
+    try {
+      const response = await tokenChecker();
+      if (response.data) {
+        console.log(response.data)
+        dispatch(regUser(response.data));
+      } else {
+        localStorage.removeItem('isAuthenticated');
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  React.useEffect(() => {
+    checkToken();
+  }, []);
 
   return (
     <>
-              <Routers />
-              </>
-         
-    //   {isLoading ? (
-    //     <></>
-    //   ) : (
-    //     <>
-    //     </>  
-    //     )}
-    // </> */}
+      {isLoading ? (
+        <></>
+      ) : (
+          <>
+            <Routers />
+          </>
+        )}
+    </>
   )
 };
 
